2919766db0a41cd8bcc9e28a7b9f7f2140d89fad
João Marcelo <jcoelhopacheco@gmail.com>
2024-03-27 13:53:02 -0300

    Tentando implantar menu

--- Software/init.py
+++ Software/init.py
@@ -12,9 +12,6 @@ import scipy as scp
 import matplotlib.pyplot as plt
 from pHcalc import Acid, Inert, System
 
-# Components
-import cmean
-
 def main():
     app = Tk()
     gui = main_window(app)
@@ -22,12 +19,23 @@ def main():
     return None
 
 class main_window:
-    def __init__(self, app):
+    def Nulo():
+        print("")
+
+    def __init__(self, app, menubar):
         self.app = app
         self.app.title("Advanced Calculator for Quantitative Analytical Calculations")
         self.app.geometry('500x700')
+        self.app(Menu = menubar)
 
-class menubar:
-    pass
+    def menu(self, app, Nulo):
+        menubar = Menu(self.app)
+        mfile = Menu(menubar, tearoff=0)
+        mfile.add_command(label='New',command=Nulo)
+        mfile.add_command(label='Save as',command=Nulo)
+        mfile.add_command(label='Clear',command=Nulo)
+        mfile.add_command(label='Print',command=Nulo)
+        mfile.add_separator()
+        mfile.add_command(label='exit',command=self.app.quit)
 
 main()
