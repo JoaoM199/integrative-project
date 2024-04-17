2b044864511c73b99654ef09d316c87539291af7
João Marcelo <jcoelhopacheco@gmail.com>
2024-04-17 20:10:32 -0300

    Algorítimo para curva de titulação foi criado com sucesso. Aplicativo foi movido para a pasta principal do projeto e a subpasta foi deletada

--- init.py
+++ init.py
@@ -12,7 +12,8 @@ from scipy.optimize import fsolve
 import numpy as np
 import scipy as scp
 import matplotlib.pyplot as plt
-#from pHcalc import Acid, Inert, System # By Ryan Nelson
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg # Plotar gráfico dentro de uma janela
+from matplotlib import figure
             
 def cmean():
     Label(tab_mean, text = "Enter comma-separated values (','): ", anchor=W).place(x=10,y=30, width=300, height=20)
@@ -36,7 +37,7 @@ def cmean():
                 
         Label(tab_mean, text='x = {}'.format(val),anchor=W).place(x=10,y=70, width=300, height=20)
         Label(tab_mean, text='n = {}'.format(n),anchor=W).place(x=10,y=90, width=300, height=20)
-        Label(tab_mean, text='mean = {}'.format(mean),anchor=W, foreground="#00a", font="Bold").place(x=10,y=110, width=300, height=20)
+        Label(tab_mean, text='mean = {}'.format(mean),anchor=W, foreground="#00a").place(x=10,y=110, width=300, height=20)
 
     calculate = Button(tab_mean, text="Calculate", command=calc_mean)
     calculate.place(x=10,y=130,width=300, height=20)
@@ -57,7 +58,7 @@ def cdev():
 
         Label(tab_dev, text='x = {}'.format(val), anchor=W).place(x=10, y=110, width=300, height=20)
         Label(tab_dev, text='d = {}'.format(devitation_values), anchor=W).place(x=10,y=130, width=450, height=20)
-        Label(tab_dev, text='mean of d = {}'.format(devitation_mean), anchor=W, foreground='#00a', font={'bold'}).place(x=10,y=160,width=300, height=20)
+        Label(tab_dev, text='mean of d = {}'.format(devitation_mean), anchor=W, foreground='#00a').place(x=10,y=160,width=300, height=20)
     calculate = Button(tab_dev, text="Calculate", command=calc_devitation)
     calculate.place(x=10,y=90,width=300, height=20)
 def csd():
@@ -84,10 +85,8 @@ def csd():
         Label(tab_csd, text='mean = {}'.format(mean), anchor=W).place(x=10,y=150,width=300, height=20)
 
         # Values
-        Label(tab_csd, text='The Standard Deviation is equal to', anchor=W).place(x=10,y=170, width=300, height=20)
-        Label(tab_csd, text=sd, anchor=W, foreground='#00a', font={'bold'}).place(x=10,y=190, width=300, height=20)
-        Label(tab_csd, text='The Relative Standard Detation os equal to', anchor=W).place(x=10,y=210, width=300, height=20)
-        Label(tab_csd, text=rsd, anchor=W, foreground='#00a', font={'bold'}).place(x=10,y=230, width=300, height=20)
+        Label(tab_csd, text='sd = {}'.format(sd), anchor=W, foreground='#00a').place(x=10,y=170, width=300, height=20)
+        Label(tab_csd, text='rsd = {}'.format(rsd), anchor=W, foreground='#00a').place(x=10,y=190, width=300, height=20)
 
     calculate = Button(tab_csd, text="Calculate", command=calc_sd)
     calculate.place(x=10,y=50,width=300, height=20)
@@ -117,11 +116,10 @@ def ttr():
         u = (mean - error_margin, mean + error_margin)
 
         Label(tab_ttr, text = 'The trust threshold of {},\n with t = {} is equal to:'.format(fval,t/100), anchor=W).place(x=10,y=110,width=450,height=20)
-        Label(tab_ttr, text='u = {}'.format(u), anchor=W, foreground="#00a", font="Bold").place(x=10,y=140, width=450, height=20)
+        Label(tab_ttr, text='u = {}'.format(u), anchor=W, foreground="#00a").place(x=10,y=140, width=450, height=20)
 
     calculate = Button(tab_ttr, text="Calculate", command=calc_ttr)
     calculate.place(x=10,y=90,width=300, height=20)
-
 def phc():
     def error_non_numeric():
         Label(tab_ph, text = "ERROR: Non numeric value", anchor=W, foreground='#a00').place(x=100,y=170,width=450,height=20)
@@ -572,16 +570,16 @@ def phc():
             H, OH = fsolve(eq, (1e-7, 1e-7))
 
             # Resultado
-            Label(tab_ph, text = 'pH = {}'.format(H), anchor=W, foreground='#00a').place(x=100,y=320,width=450,height=20)
-            Label(tab_ph, text = 'pOH = {}'.format(OH), anchor=W, foreground='#00a').place(x=100,y=340,width=450,height=20)
+            Label(tab_ph, text = 'pH = {}'.format(H), anchor=W, foreground='#00a').place(x=100,y=350,width=450,height=20)
+            Label(tab_ph, text = 'pOH = {}'.format(OH), anchor=W, foreground='#00a').place(x=100,y=370,width=450,height=20)
 
             # Imprimindo valores
-            Label(tab_ph, text='Volume of acid: {}'.format(Ac_vol*1000), anchor=W).place(x=100,y=200,width=450,height=20)
-            Label(tab_ph, text='Volume of base: {}'.format(B_vol*1000), anchor=W).place(x=100,y=220,width=450,height=20)
-            Label(tab_ph, text='Concentration of acid: {}'.format(Ac_con), anchor=W).place(x=100,y=240,width=450,height=20)
-            Label(tab_ph, text='Concentration of acid: {}'.format(B_con), anchor=W).place(x=100,y=260,width=450,height=20)
-            Label(tab_ph, text='Ka: {}'.format(ka), anchor=W).place(x=100,y=280,width=450,height=20)
-            Label(tab_ph, text='Kb: {}'.format(kb), anchor=W).place(x=100,y=300,width=450,height=20)
+            Label(tab_ph, text='Volume of acid: {}'.format(Ac_vol*1000), anchor=W).place(x=100,y=230,width=450,height=20)
+            Label(tab_ph, text='Volume of base: {}'.format(B_vol*1000), anchor=W).place(x=100,y=250,width=450,height=20)
+            Label(tab_ph, text='Concentration of acid: {}'.format(Ac_con), anchor=W).place(x=100,y=270,width=450,height=20)
+            Label(tab_ph, text='Concentration of acid: {}'.format(B_con), anchor=W).place(x=100,y=290,width=450,height=20)
+            Label(tab_ph, text='Ka: {}'.format(ka), anchor=W).place(x=100,y=310,width=450,height=20)
+            Label(tab_ph, text='Kb: {}'.format(kb), anchor=W).place(x=100,y=330,width=450,height=20)
 
         calculate = Button(tab_ph, text="Calculate", command=calc_ph_wab)
         calculate.place(x=100,y=210,width=300, height=20)
@@ -613,8 +611,148 @@ def phc():
             phc_weak_base()
         elif SelOption.get() == "Weak acid & weak base":
             phc_wab()
+
     SelOption.trace("w", option_changed)
     SelOption.set(Options_list[0]) # Valor padrão
+def tcurve():
+    # Ácido forte e base forte
+    def error_non_numeric():
+        Label(tab_tcurve, text = "ERROR: Non numeric value", anchor=W, foreground='#a00').place(x=100,y=170,width=450,height=20)
+    def tcurve_sab():
+        Label(tab_tcurve, text="Enter the acid volume (mL): ", anchor=W).place(x=10,y=30, width=300, height=20) # Acid volume
+        input_Ac_vol = Entry(tab_tcurve)
+        input_Ac_vol.place(x=10,y=50,width=50,height=20)
+
+        Label(tab_tcurve, text="Enter the acid concentration (N): ", anchor=W).place(x=10,y=70, width=300, height=20) # Acid concentration
+        input_Ac_con = Entry(tab_tcurve)
+        input_Ac_con.place(x=10,y=90,width=50,height=20)
+
+        Label(tab_tcurve, text="Enter the Base concentration (N)", anchor=W).place(x=300,y=70, width=300, height=20) # Base concentration
+        input_B_con = Entry(tab_tcurve)
+        input_B_con.place(x=300,y=90,width=50,height=20)
+                
+
+        def generate_tcurve_sab():
+            # Variáveis de entrada
+            try:
+                # Entrada
+                V_acid = float(input_Ac_vol.get())
+            except ValueError:
+                # Erro
+                error_non_numeric()
+
+            try:
+                # Entrada
+                C_Acid = float(input_Ac_con.get())
+            except ValueError:
+                error_non_numeric()
+
+            try:
+                # Entrada
+                C_base = float(input_B_con.get())
+            except ValueError:
+                error_non_numeric()
+
+            # Volume da base necessário para a neutralização
+            V_Base = (C_Acid * V_acid)/C_base
+            # Criação de uma lista de volumes da base a serem adicionados
+            V_Base_add = np.linspace(0, 2*V_Base, 500)
+
+            # Cálculo de pH para cada volume adicionado
+            pH = []
+            for V in V_Base_add:
+                if V < V_Base:
+                    n_Acid = C_Acid - C_base * V
+                    V_total = V_acid + V
+                    if n_Acid > 0:
+                        pH.append(-np.log10(n_Acid/V_total))
+                    else:
+                        pH.append(0)
+                elif V == V_Base:
+                    pH.append(7) # Ponto de equivalência
+                else:
+                    n_base = C_base * V - C_Acid * V_acid
+                    V_total = V_acid + V
+                    pOH = -np.log10(n_base/V_total)
+                    pH.append(14 - pOH)
+
+            # Imprimindo valores inseridos
+            Label(tab_tcurve, text='Concentration of acid: {}'.format(C_Acid), anchor=W).place(x=100,y=170,width=450,height=20)
+            Label(tab_tcurve, text='Concentration of base: {}'.format(C_base), anchor=W).place(x=100,y=190,width=450,height=20)
+            Label(tab_tcurve, text='Volume of acid: {}mL'.format(V_acid), anchor=W).place(x=100,y=210,width=450,height=20)
+
+            # Gerando gráfico
+            def show_graph():
+                plt.figure(figsize=(8,6))
+                plt.plot(V_Base_add, pH, label = 'Titration curve')
+                plt.xlabel('Base volume added (mL)')
+                plt.ylabel('pH')
+                plt.title('Titration curve')
+                plt.legend()
+                plt.grid(True)
+                plt.show()
+            
+            # Gráfico dentro de janela
+            fig = plt.figure(figsize=(8,8))
+            plt.plot(V_Base_add, pH, label = 'Titration curve')
+            plt.xlabel('Base volume added (mL)')
+            plt.ylabel('pH')
+            plt.title('Titration curve')
+            plt.legend()
+            plt.grid(True)
+                
+            graph = FigureCanvasTkAgg(fig, master=tab_tcurve)
+            graph.draw()
+            graph.get_tk_widget().place(x=10,y=250, width=600, height=200)
+            # Botão para mostrar gráfico mais detalhado
+            showmore = Button(tab_tcurve, text="Show more", command=show_graph)
+            showmore.place(x=300,y=210,width=100, height=20)
+        # Botão para calcular
+        calculate = Button(tab_tcurve, text="Calculate", command=generate_tcurve_sab)
+        calculate.place(x=100,y=130,width=300, height=20)
+    def tcurve_wa():
+        pass
+    def tcurve_wb():
+        pass
+    def tcurve_wab():
+        pass
+    def tcurve_p():
+        pass
+
+    
+    # Opções
+    Options_frame = Frame(tab_tcurve)
+    Options_frame.pack()
+    Options_list = [
+        "titration of strong acid in strong base",
+        "titration of weak acid to strong base",
+        "titration of weak base to strong acid",
+        "titration of strong acid to weak base",
+        "precipitation titration"
+    ]
+    SelOption = StringVar()
+    Options = OptionMenu(Options_frame,SelOption,*Options_list)
+    Options.pack()
+
+    def clear_tab_tcurve():
+        for widget in tab_tcurve.winfo_children():
+            if widget != Options_frame:
+                widget.destroy()
+    def option_changed(*args):
+        clear_tab_tcurve()
+        if SelOption.get() == "titration of strong acid in strong base":
+            tcurve_sab()
+        elif SelOption.get() == "titration of weak acid to strong base":
+            tcurve_wa()
+        elif SelOption.get() == "titration of weak base to strong acid":
+            tcurve_wb()
+        elif SelOption.get() == "titration of strong acid to weak base":
+            tcurve_wab()
+        elif SelOption.get() == "precipitation titration":
+            tcurve_p()
+
+    SelOption.trace("w", option_changed)
+    SelOption.set(Options_list[0])
 
 
 # Apagar esta função quando finalizar
@@ -625,7 +763,7 @@ def Nulo():
 ######################################### Main Window #################################################################
 app = Tk()
 app.title("Advanced Calculator for Quantitative Analytical Calculations")
-app.geometry('500x700')
+app.geometry('700x900')
 
 # Add tabs
 tabs = ttk.Notebook(app)
@@ -651,10 +789,15 @@ tab_ttr.pack(fill="both", expand=1)
 ttr()
 tabs.add(tab_ttr, text="trust threshold")
 
-tab_ph = Frame(tabs, width=500, height=300)
+tab_ph = Frame(tabs, width=600, height=600)
 tab_ph.pack(fill='both', expand=1)
 phc()
 tabs.add(tab_ph, text="pH & pOH")
+
+tab_tcurve = Frame(tabs, width=700, height=900)
+tab_tcurve.pack(fill="both",expand=1)
+tcurve()
+tabs.add(tab_tcurve, text="Titration curve")
 
 # Menubar
 menubar = Menu(app)
